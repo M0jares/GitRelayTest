26c84d37decb8312f68b11eb37ca55516da15be9
Ionics-Dharelle <dharelle.sison@ionics-ems.com>
2024-06-18 10:08:55 +0800

    Testing

--- MsNumatoRelay/obj/Debug/net8.0/MsNumatoRelay.AssemblyInfo.cs
+++ MsNumatoRelay/obj/Debug/net8.0/MsNumatoRelay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MsNumatoRelay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a719177c410d2cd38b94b3b8659bd77edbab76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad5293df6be37daeaba7063bfe40b0f62c621189")]
 [assembly: System.Reflection.AssemblyProductAttribute("MsNumatoRelay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MsNumatoRelay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
